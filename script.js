bfedfd76d5d77e3bfe5e48644c78b96188ef1b7c
AyushiPohekar <ayushi2021.29@gmail.com>
2022-09-17 20:40:32 +0530

    before clicked

--- script.js
+++ script.js
@@ -1,152 +1,173 @@
-
-
-
 // bootstrap container
-var container=document.createElement('div');
-container.setAttribute('class','container');
-
+var container = document.createElement("div");
+container.setAttribute("class", "container");
 
 // bootstrap row
-var row=document.createElement('div');
-row.setAttribute('class','row');
-
-
+var row = document.createElement("div");
+row.setAttribute("class", "row");
 
 // pagination logic
-var cur_page=0;
-var records_per_page=25;
-var max_pages=Math.ceil(250/records_per_page);
+var cur_page = 0;
+var records_per_page = 25;
+var max_pages = Math.ceil(250 / records_per_page);
 
-function prev_page()
-{
-    if(cur_page>1)
-    {
-        changePage(cur_page-1);
-    }
+function prev_page() {
+  if (cur_page > 1) {
+    changePage(cur_page - 1);
+  }
 }
-function next_page()
-{
-    if(cur_page<max_pages)
-    {
-        changePage(cur_page+1);
-    }
+function next_page() {
+  if (cur_page < max_pages) {
+    changePage(cur_page + 1);
+  }
 }
 
-function changePage(num)
-{
-    if(num<1) num=1;
-    
-    if(num>max_pages)
-        num=max_pages;
-    
+function changePage(num) {
+  if (num < 1) num = 1;
 
-    var startpoint=(num-1)*records_per_page;
-    var endpoint=(num)*records_per_page;
+  if (num > max_pages) num = max_pages;
 
-    cur_page=num;
-    Create_data_container(startpoint,endpoint);
+  var startpoint = (num - 1) * records_per_page;
+  var endpoint = num * records_per_page;
 
-    if (num === 1) {
-        document.getElementById('prev').style.visibility = "hidden";
-    } else {
-        document.getElementById('prev').style.visibility = "visible";
-    }
+  cur_page = num;
+  Create_data_container(startpoint, endpoint);
 
-    if (num === max_pages) {
-        document.getElementById('prev').style.visibility = "hidden";
-    } else {
-        document.getElementById('next').style.visibility = "visible";
-    }
+  if (num === 1) {
+    document.getElementById("prev").style.visibility = "hidden";
+  } else {
+    document.getElementById("prev").style.visibility = "visible";
+  }
+
+  if (num === max_pages) {
+    document.getElementById("prev").style.visibility = "hidden";
+  } else {
+    document.getElementById("next").style.visibility = "visible";
+  }
 }
 
-function Create_data_container(start,end){
-    row.innerHTML=' ';
- async function get_weather_data() {
-        
-        try {
-            let response = await fetch("https://restcountries.com/v3.1/all");
-            console.log(response);
-            let data = await response.json();
-            console.log(data);
-            
-            
-            for(var i=start;i<end;i++)
-            {
-                var card = document.createElement('div');
-                card.setAttribute('class','card row col-lg-4 col-md-6 col-sm-12');
-                 card.setAttribute("id","cards");
+function Create_data_container(start, end) {
+  row.innerHTML = " ";
+  async function get_weather_data() {
+    try {
+      let response = await fetch("https://restcountries.com/v3.1/all");
+      console.log(response);
+      let data = await response.json();
+      console.log(data);
 
-           
-                var header = document.createElement('h4')
-                header.setAttribute('class', 'card-title text-center  text-black')
-                header.innerHTML = data[i].name.common;
+      for (var i = start; i < end; i++) {
+        var card = document.createElement("div");
+        card.setAttribute("class", "card row col-lg-4 col-md-6 col-sm-12");
+        card.setAttribute("id", "cards");
 
-                console.log(data[i].flags.png);
+        var header = document.createElement("h4");
+        header.setAttribute("class", "card-title text-center");
+        header.innerHTML = data[i].name.common;
 
+        // var cardbody = document.createElement('div');
+        // card.setAttribute('class','card-body');
 
-                row.append(card);
-                container.append(row);
-                card.append(header);
-              
-                
-           
-            
-            }
+        var img = document.createElement("img");
+        img.setAttribute("class", "card-img-top img-center");
+        img.src = data[i].flags.png;
 
+        var capital = document.createElement("h6");
+        capital.setAttribute("class", "card-text text-center");
+        capital.innerHTML = "Capital : " + data[i].capital;
 
-        
-         
-        }
-        catch (error) {
-            console.log(error);
-        }
+        var region = document.createElement("h6");
+        region.setAttribute("class", "card-text text-center");
+        region.innerHTML = "Region : " + data[i].region;
+
+        var code = document.createElement("h6");
+        code.setAttribute("class", "card-text text-center");
+        code.innerHTML = "Country-code : " + data[i].cca3;
+
+        var climate = document.createElement("h6");
+        climate.setAttribute("class", "card-text text-center mt-3");
+        // climate.innerHTML='hello';
+
+        var button = document.createElement("button");
+        button.setAttribute("class", "btn-btn-info button");
+        console.log('before fetching:',i);
+        button.innerHTML = "Click for Weather";
+        button.setAttribute('id',i);
+        button.onclick = async function () {
+          try {
+            console.log(i);
+            var response1 = await fetch(
+              `https://api.openweathermap.org/data/2.5/weather?lat=${data[i].latlng[0]}&lon=${data[i].latlng[1]}&appid=9886709d311837a96f411b3ed9f3b47e`
+            );
+            var data1 = await response1.json();
+            console.log(data1);
+            //  climate.innerHTML=data1.main.temp;
+
+            climate.innerHTML = "hello";
+          } catch (err) {
+            console.log(err);
+          }
+        };
+
+        row.append(card);
+        container.append(row);
+        card.append(header, img, capital, region, code, button, climate);
+        // cardbody.append(capital);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    get_weather_data();
+  }
+  get_weather_data();
 }
-
-
 
 // Anchorlist
 
-var Anchorlist=document.createElement("div");
-Anchorlist.setAttribute('class','anchorlist');
+var Anchorlist = document.createElement("div");
+Anchorlist.setAttribute("class", "anchorlist");
 
-var prev=document.createElement('a');
-prev.href=`javascript:prev_page`;
-prev.id='prev';
-prev.innerHTML='&laquo;';
+var prev = document.createElement("a");
+prev.href = `javascript:prev_page`;
+prev.id = "prev";
+prev.innerHTML = "&laquo;";
 
-var next=document.createElement('a');
-next.href=`javascript:next_page`;
-next.id='next';
-next.innerHTML="&raquo;";
+var next = document.createElement("a");
+next.href = `javascript:next_page`;
+next.id = "next";
+next.innerHTML = "&raquo;";
 
-var arr=createAnchorlist();
+var arr = createAnchorlist();
 
-function createAnchorlist()
-{
-    var  ar=[];
-    for(var i=1;i<=11;i++)
-    {
-        var a=document.createElement('a');
-        a.href=`javascript:changePage(${i})`;
-        a.innerHTML=i;
-        if(i===1)
-        {
-            a.setAttribute("class","active");
-        }
-        ar.push(a);
+function createAnchorlist() {
+  var ar = [];
+  for (var i = 1; i <= 11; i++) {
+    var a = document.createElement("a");
+    a.href = `javascript:changePage(${i})`;
+    a.innerHTML = i;
+    if (i === 1) {
+      a.setAttribute("class", "active");
     }
-    return ar;
+    ar.push(a);
+  }
+  return ar;
 }
-Anchorlist.append(prev, arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9],arr[10],next)
-document.body.append(Anchorlist,container);
+Anchorlist.append(
+  prev,
+  arr[0],
+  arr[1],
+  arr[2],
+  arr[3],
+  arr[4],
+  arr[5],
+  arr[6],
+  arr[7],
+  arr[8],
+  arr[9],
+  arr[10],
+  next
+);
+document.body.append(container, Anchorlist);
 
 changePage(1);
-
-
-
-
 
 // async function get_weather_data() {
 //     try {
